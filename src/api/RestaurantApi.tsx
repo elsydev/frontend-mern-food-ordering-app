e20185ec015d4615cd6ce9a4884454a68da0d615
elsydev <interelsy2@gmail.com>
2024-08-03 13:41:36 -0400

    Add files via upload

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -4,17 +4,31 @@ import { SearchState } from "../pages/SearchPage"
 
 
 const API_BASE_URL= import.meta.env.VITE_API_BASE_URL;
-export const useGetRestaurant=(restaurantId?:string)=>{
-    const getRestaurantByIdRequest=async():Promise<Restaurant>=>{
-        const response =await fetch(`${API_BASE_URL}/api/restaurant/${restaurantId}`);
-        if(!response.ok){
-            throw new Error(`Error getting restaurant`)
-        }
-        return response.json()
-    };
-    const {data:restaurant, isLoading}=useQuery("fetchRestaurant",getRestaurantByIdRequest);
-    return{restaurant,isLoading};
-}
+export const useGetRestaurant = (restaurantId?: string) => {
+  console.log(restaurantId)
+  const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/${restaurantId}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+
+    return response.json();
+  };
+
+  const { data: restaurant, isLoading } = useQuery(
+    "fetchRestaurant",
+    getRestaurantByIdRequest,
+    {
+      enabled: !!restaurantId,
+    }
+  );
+
+  return { restaurant, isLoading };
+};
+
 export const useSearchRestaurants=(searchState:SearchState,city?:string)=>{
         const createSearchRequest= async():Promise<RestaurantSearchResponse>=>{
             const params = new URLSearchParams();
